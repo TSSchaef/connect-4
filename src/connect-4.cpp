c5bf2977a3767a6d51acb39a162eb3d0a1074c79
TSSchaef <tschaeferstorm@gmail.com>
2024-05-23 12:07:31 -0500

    Wrote more of the board

--- src/connect-4.cpp
+++ src/connect-4.cpp
@@ -4,20 +4,53 @@
 #define HEIGHT 6
 
 class board{
-private:
-    uint8_t baord[WIDTH][HEIGHT];
+public:
+    uint8_t board[WIDTH][HEIGHT];
     uint8_t currPlayer;
 
-public:
     board(){
         //initialize as empty
+        int i, j;
+        for(i = 0; i < WIDTH; i++){
+            for(j = 0; j < HEIGHT; j++){
+                board[i][j] = 0;
+            }
+        }
+        currPlayer = 1;
     }
+
+    board(board &toCopy){
+        int i, j;
+        for(i = 0; i < WIDTH; i++){
+            for(j = 0; j < HEIGHT; j++){
+                this->board[i][j] = toCopy.board[i][j];
+            }
+        }
+        this->currPlayer = toCopy.currPlayer;
+    }
+
     ~board(){}
-
-    bool addChip(uint8_t column){
-        return true;
+    
+    //switches current player - returns current player
+    int switchPlayer(){
+       currPlayer = (currPlayer == 1) ? 2 : 1;
+       return currPlayer;
     }
 
+    //does nothing if chip cannot be added
+    bool addChip(uint8_t column){
+        uint8t i;
+        for(i = 0; i < HEIGHT; i++){
+            if(board[column][i] == 0){
+                board[column][i] = currPlayer;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //returns if a connect 4 was completed
+    //column is the last column where a chip was played
     uint8_t gameOver(uint8_t column){
         return 0;
     }
