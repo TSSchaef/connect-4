44913f6c5b7f781e91bab26291bcb87f271c9918
TSSchaef <tschaeferstorm@gmail.com>
2025-07-07 19:32:47 -0500

    Cleaned game UX

--- src/connect-4.c
+++ src/connect-4.c
@@ -15,10 +15,14 @@
 #define EMPTY_TILE "src/images/empty-tile.png"
 #define RED_TILE "src/images/red-tile.png"
 #define YELLOW_TILE "src/images/yellow-tile.png"
+#define PREVIEW_RED "src/images/red.png"
+#define PREVIEW_YELLOW "src/images/yellow.png"
 
-#define TILE_SIZE 100
-#define SCRN_WIDTH (TILE_SIZE*WIDTH)
-#define SCRN_HEIGHT (TILE_SIZE*(HEIGHT + 1))
+#define BASE_TILE_SIZE 100
+#define BASE_MENU_HEIGHT 56
+#define BASE_STATUS_HEIGHT 52
+#define WIDTH 7
+#define HEIGHT 6
 
 typedef enum { STATE_PLAYING, STATE_GAMEOVER } GameState;
 GameState gameState = STATE_PLAYING;
@@ -29,46 +33,112 @@ SDL_Renderer *renderer;
 bool keysPressed[SDL_NUM_SCANCODES];
 
 SDL_Texture *emptyTile, *redTile, *yellowTile;
-SDL_Rect bckgrndRect = {0, 0, SCRN_WIDTH, SCRN_HEIGHT};
-
-TTF_Font* Hyperspace; 
+SDL_Texture *previewRed, *previewYellow;
+TTF_Font* Hyperspace;
 
 board_t game;
-uint8_t currColumn = 3; 
+uint8_t currColumn = 3;
 
 int shouldQuit = 0;
 uint32_t gameOverTicks = 0;
 uint32_t tieGameTicks = 0;
+
+int window_width = BASE_TILE_SIZE * WIDTH;
+int window_height = BASE_TILE_SIZE * HEIGHT + BASE_MENU_HEIGHT + BASE_STATUS_HEIGHT + BASE_TILE_SIZE;
+
+enum InputSource { INPUT_NONE, INPUT_MOUSE, INPUT_KEYBOARD };
+enum InputSource lastInputSource = INPUT_NONE;
+uint32_t lastInputTimestamp = 0;
+
+int mouseColumn = -1;
+
+void get_layout(int* tile_size, int* menu_height, int* status_height, int* preview_y, int* status_y, int* board_y) {
+    *menu_height = BASE_MENU_HEIGHT * window_width / (BASE_TILE_SIZE * WIDTH);
+    *status_height = BASE_STATUS_HEIGHT * window_width / (BASE_TILE_SIZE * WIDTH);
+
+    int remaining_height = window_height - *menu_height - *status_height;
+    int tile_size_x = window_width / WIDTH;
+    int tile_size_y = remaining_height / (HEIGHT + 1);
+    *tile_size = (tile_size_x < tile_size_y) ? tile_size_x : tile_size_y;
+
+    int used_height = *tile_size * (HEIGHT + 1);
+    int top_offset = *menu_height + ((remaining_height - used_height) > 0 ? (remaining_height - used_height)/2 : 0);
+
+    *preview_y = top_offset;
+    *status_y = *menu_height;
+    *board_y = top_offset + *tile_size;
+}
 
 int userInput() {
     SDL_Event event;
     while(SDL_PollEvent(&event)){
         switch (event.type){
             case SDL_QUIT:
+#ifndef __EMSCRIPTEN__
                 shouldQuit = 1;
+#endif
                 return 0;
             case SDL_KEYDOWN:
-                if(event.key.keysym.scancode == SDL_SCANCODE_Q){
-                    shouldQuit = 1;
 #ifdef __EMSCRIPTEN__
-            emscripten_cancel_main_loop();
+                if(event.key.keysym.scancode == SDL_SCANCODE_R) {
+#else
+                if(event.key.keysym.scancode == SDL_SCANCODE_R || event.key.keysym.scancode == SDL_SCANCODE_Q) {
 #endif
-                    return 0;
-                }
-                if (gameState == STATE_GAMEOVER && event.key.keysym.scancode == SDL_SCANCODE_R) {
-                    // Restart game
-                    init_board(&game);
-                    currColumn = 3;
-                    gameOverTicks = 0;
-                    tieGameTicks = 0;
-                    gameState = STATE_PLAYING;
-                    printf("Restarting game!\n");
+#ifndef __EMSCRIPTEN__
+                    if(event.key.keysym.scancode == SDL_SCANCODE_Q){
+                        shouldQuit = 1;
+                        return 0;
+                    }
+#endif
+                    if(event.key.keysym.scancode == SDL_SCANCODE_R) {
+                        init_board(&game);
+                        currColumn = 3;
+                        gameOverTicks = 0;
+                        tieGameTicks = 0;
+                        mouseColumn = -1;
+                        gameState = STATE_PLAYING;
+                        lastInputSource = INPUT_NONE;
+                        for (int i = 0; i < SDL_NUM_SCANCODES; ++i) keysPressed[i] = false;
+                        break;
+                    }
                 }
                 keysPressed[event.key.keysym.scancode] = true;
-            break;
+                lastInputSource = INPUT_KEYBOARD;
+                lastInputTimestamp = SDL_GetTicks();
+                break;
             case SDL_KEYUP:
                 keysPressed[event.key.keysym.scancode] = false;
-            break;
+                break;
+            case SDL_MOUSEMOTION: {
+                int tile_size, menu_height, status_height, preview_y, status_y, board_y;
+                get_layout(&tile_size, &menu_height, &status_height, &preview_y, &status_y, &board_y);
+                int x = event.motion.x;
+                int col = (x >= 0) ? (x / tile_size) : 0;
+                if (col < 0) col = 0;
+                if (col >= WIDTH) col = WIDTH - 1;
+                mouseColumn = col;
+                lastInputSource = INPUT_MOUSE;
+                lastInputTimestamp = SDL_GetTicks();
+                break;
+            }
+            case SDL_MOUSEBUTTONDOWN:
+                if (event.button.button == SDL_BUTTON_LEFT && gameState == STATE_PLAYING) {
+                    int col = (lastInputSource == INPUT_MOUSE && mouseColumn >= 0) ? mouseColumn : currColumn;
+                    if (col >= 0 && canAdd(&game, col)) {
+                        addChip(&game, col);
+                        if (!game.gameOver && game.moves < WIDTH * HEIGHT) {
+                            int input = computerInput(game);
+                            addChip(&game, input);
+                        }
+                    }
+                }
+                break;
+            case SDL_WINDOWEVENT:
+                if (event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED) {
+                    window_width = event.window.data1;
+                    window_height = event.window.data2;
+                }
+                break;
         }
     }
     return 1;
@@ -76,88 +146,147 @@ int userInput() {
 
 void updateGame(){
     static bool justMoved = false;
-    if(keysPressed[SDL_SCANCODE_A] || keysPressed[SDL_SCANCODE_LEFT]){
-        if(!justMoved && currColumn > 0) currColumn--;
-        justMoved = true;
-    } else if(keysPressed[SDL_SCANCODE_D] || keysPressed[SDL_SCANCODE_RIGHT]){
-        if(!justMoved && currColumn < WIDTH - 1) currColumn++;
-        justMoved = true;
-    } else if(keysPressed[SDL_SCANCODE_SPACE] || keysPressed[SDL_SCANCODE_S] 
-            || keysPressed[SDL_SCANCODE_DOWN]){
-        if(!justMoved && canAdd(&game, currColumn)){
-            addChip(&game, currColumn);
-            if(!game.gameOver){
-                int input = computerInput(game);
-                addChip(&game, input); 
+    if (lastInputSource == INPUT_KEYBOARD) {
+        if(keysPressed[SDL_SCANCODE_A] || keysPressed[SDL_SCANCODE_LEFT]){
+            if(!justMoved && currColumn > 0) currColumn--;
+            justMoved = true;
+        } else if(keysPressed[SDL_SCANCODE_D] || keysPressed[SDL_SCANCODE_RIGHT]){
+            if(!justMoved && currColumn < WIDTH - 1) currColumn++;
+            justMoved = true;
+        } else if(keysPressed[SDL_SCANCODE_SPACE] || keysPressed[SDL_SCANCODE_S]
+                  || keysPressed[SDL_SCANCODE_DOWN]){
+            if(!justMoved && canAdd(&game, currColumn)){
+                addChip(&game, currColumn);
+                if(!game.gameOver && game.moves < WIDTH * HEIGHT){
+                    int input = computerInput(game);
+                    addChip(&game, input);
+                }
             }
+            justMoved = true;
+        } else {
+            justMoved = false;
         }
-        justMoved = true;
-    } else {
-        justMoved = false;
+    }
+    if (lastInputSource == INPUT_MOUSE && mouseColumn >= 0) {
+        currColumn = mouseColumn;
     }
 }
 
+// --- Draw the menu at the top ---
+void drawMenu() {
+    int tile_size, menu_height, status_height, preview_y, status_y, board_y;
+    get_layout(&tile_size, &menu_height, &status_height, &preview_y, &status_y, &board_y);
+
+    if (!Hyperspace) return;
+
+    SDL_Color yellow = {255, 255, 100, 255};
+    char menuText[256];
+#ifdef __EMSCRIPTEN__
+    if (gameState == STATE_GAMEOVER) {
+        snprintf(menuText, sizeof(menuText), "R to restart");
+    } else {
+        snprintf(menuText, sizeof(menuText),
+            "←/A, →/D or Mouse to select   |   Space/Click to drop   |   R to restart");
+    }
+#else
+    if (gameState == STATE_GAMEOVER) {
+        snprintf(menuText, sizeof(menuText), "R to restart   |   Q to quit");
+    } else {
+        snprintf(menuText, sizeof(menuText),
+            "←/A, →/D or Mouse to select   |   Space/Click to drop   |   R to restart   |   Q to quit");
+    }
+#endif
+
+    SDL_Surface* surface = TTF_RenderUTF8_Blended_Wrapped(Hyperspace, menuText, yellow, window_width - 12);
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_Rect rect;
+    rect.x = 6;
+    rect.y = 0;
+    rect.w = (surface->w > window_width-12) ? window_width-12 : surface->w;
+    rect.h = surface->h;
+    SDL_RenderCopy(renderer, texture, NULL, &rect);
+    SDL_FreeSurface(surface);
+    SDL_DestroyTexture(texture);
+}
+
+// --- Draw the preview chip above the board using new PNGs ---
 void drawSelection(){
-    SDL_Rect tile = {currColumn * TILE_SIZE, 0, TILE_SIZE, TILE_SIZE};
-    if(game.moves % 2 == 0){
-        if (SDL_RenderCopy(renderer, redTile, NULL, &tile) != 0) {
-            printf("SDL_RenderCopy (drawSelection, redTile) error: %s\n", SDL_GetError());
-        }
-    } else {
-        if (SDL_RenderCopy(renderer, yellowTile, NULL, &tile) != 0) {
-            printf("SDL_RenderCopy (drawSelection, yellowTile) error: %s\n", SDL_GetError());
+    int tile_size, menu_height, status_height, preview_y, status_y, board_y;
+    get_layout(&tile_size, &menu_height, &status_height, &preview_y, &status_y, &board_y);
+    SDL_Rect tile = {currColumn * tile_size, preview_y, tile_size, tile_size};
+    SDL_Texture* preview = (game.moves % 2 == 0) ? previewRed : previewYellow;
+    SDL_RenderCopy(renderer, preview, NULL, &tile);
+}
+
+// --- Draw the board using the original PNGs ---
+void drawBoard(){
+    int tile_size, menu_height, status_height, preview_y, status_y, board_y;
+    get_layout(&tile_size, &menu_height, &status_height, &preview_y, &status_y, &board_y);
+    for(int i = 0; i < WIDTH; i++){
+        for(int j = 0; j < HEIGHT; j++){
+            SDL_Rect tile = {
+                i * tile_size,
+                board_y + (HEIGHT - 1 - j) * tile_size,
+                tile_size, tile_size
+            };
+            if(game.position & (BOTTOM_LEFT << (j + (i * (HEIGHT + 1))))){
+                SDL_RenderCopy(renderer, (game.moves % 2 == 0) ? redTile : yellowTile, NULL, &tile);
+            } else if(game.mask & (BOTTOM_LEFT << (j + (i * (HEIGHT + 1))))){
+                SDL_RenderCopy(renderer, (game.moves % 2 == 1) ? redTile : yellowTile, NULL, &tile);
+            } else {
+                SDL_RenderCopy(renderer, emptyTile, NULL, &tile);
+            }
         }
     }
 }
 
-void drawBoard(){
-    int i, j;
-    for(i = 0; i < WIDTH; i++){
-        for( j = 0; j < HEIGHT; j++){
-            SDL_Rect tile = {i * TILE_SIZE, SCRN_HEIGHT - (j * TILE_SIZE) - TILE_SIZE, TILE_SIZE, TILE_SIZE}; 
-            if(game.position & (BOTTOM_LEFT << (j + (i * (HEIGHT + 1))))){
-                if (SDL_RenderCopy(renderer, (game.moves % 2 == 0) ? redTile : yellowTile, NULL, &tile) != 0) {
-                    printf("SDL_RenderCopy (drawBoard, pos) error: %s\n", SDL_GetError());
-                }
-            } else if(game.mask & (BOTTOM_LEFT << (j + (i * (HEIGHT + 1))))){
-                if (SDL_RenderCopy(renderer, (game.moves % 2 == 1) ? redTile : yellowTile, NULL, &tile) != 0) {
-                    printf("SDL_RenderCopy (drawBoard, mask) error: %s\n", SDL_GetError());
-                }
-            } else {
-                if (SDL_RenderCopy(renderer, emptyTile, NULL, &tile) != 0) {
-                    printf("SDL_RenderCopy (drawBoard, emptyTile) error: %s\n", SDL_GetError());
-                }
-            }
-        }
+// --- Draw game over status message under the menu bar (if game ended), centered ---
+void drawStatusBar() {
+    if (gameState != STATE_GAMEOVER) return;
+    int tile_size, menu_height, status_height, preview_y, status_y, board_y;
+    get_layout(&tile_size, &menu_height, &status_height, &preview_y, &status_y, &board_y);
+
+    const char* msg = NULL;
+    SDL_Color color;
+    if (game.mask == ((1ULL << ((WIDTH)*(HEIGHT+1)))-1)) {
+        msg = "Tie Game!";
+        color.r = 200; color.g = 200; color.b = 255; color.a = 255;
+    } else if ((game.moves % 2) == 0) {
+        msg = "Yellow Won!";
+        color.r = 255; color.g = 255; color.b = 50; color.a = 255;
+    } else {
+        msg = "Red Won!";
+        color.r = 255; color.g = 60; color.b = 60; color.a = 255;
+    }
+    if (Hyperspace && msg) {
+        SDL_Surface *surface = TTF_RenderUTF8_Blended_Wrapped(Hyperspace, msg, color, window_width - 12);
+        SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+        SDL_Rect rect;
+        rect.x = (window_width - surface->w) / 2;
+        if (rect.x < 0) rect.x = 0;
+        rect.y = status_y;
+        rect.w = surface->w;
+        rect.h = surface->h;
+        SDL_RenderCopy(renderer, texture, NULL, &rect);
+        SDL_FreeSurface(surface);
+        SDL_DestroyTexture(texture);
     }
 }
 
 void render(){
-    if (SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255) != 0) {
+    if (SDL_SetRenderDrawColor(renderer, 0, 0, 40, 255) != 0) {
         printf("SDL_SetRenderDrawColor error: %s\n", SDL_GetError());
     }
     if (SDL_RenderClear(renderer) != 0) {
         printf("SDL_RenderClear error: %s\n", SDL_GetError());
     }
+    drawMenu();
+    drawStatusBar();
     drawSelection();
     drawBoard();
-    if (gameState == STATE_GAMEOVER) {
-        if(Hyperspace){
-            SDL_Color white = {255, 255, 255, 255};
-            SDL_Surface *surface = TTF_RenderText_Solid(Hyperspace, "Press R to restart or Q to quit", white);
-            SDL_Texture *msg = SDL_CreateTextureFromSurface(renderer, surface);
-            SDL_Rect msgRect = {40, SCRN_HEIGHT/2, surface->w, surface->h};
-            SDL_RenderCopy(renderer, msg, NULL, &msgRect);
-            SDL_FreeSurface(surface);
-            SDL_DestroyTexture(msg);
-        } else {
-            fprintf(stderr, "Failed to load font, press R to restart");
-        }
-    }
     SDL_RenderPresent(renderer);
 }
 
-// --- New: game loop frame function for Emscripten ---
 void gameLoopFrame(){
     if (shouldQuit) {
 #ifdef __EMSCRIPTEN__
@@ -166,14 +295,12 @@ void gameLoopFrame(){
         return;
     }
 
-    // Only process input if game is not over
     if (!game.gameOver && game.moves < WIDTH * HEIGHT) {
         userInput();
         updateGame();
         render();
     } else if(gameState == STATE_PLAYING){
         render();
-        // Set game state to game over, so we can process restart/quit input
         gameState = STATE_GAMEOVER;
         uint32_t now = SDL_GetTicks();
         if(game.gameOver && !gameOverTicks) {
@@ -185,7 +312,6 @@ void gameLoopFrame(){
             printf("Tie game!\n");
         }
     } else if(gameState == STATE_GAMEOVER) {
-        // Allow input for restart or quit
         userInput();
         render();
     }
@@ -196,7 +322,12 @@ int startScreen(){
         printf("SDL_Init error: %s\n", SDL_GetError());
         return -1;
     }
-    window = SDL_CreateWindow("Connect-4", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCRN_WIDTH, SCRN_HEIGHT, 0);
+    window = SDL_CreateWindow(
+        "Connect-4",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        window_width, window_height,
+        SDL_WINDOW_RESIZABLE
+    );
     if (!window) {
         printf("SDL_CreateWindow error: %s\n", SDL_GetError());
         return -1;
@@ -220,10 +351,19 @@ int startScreen(){
         printf("IMG_LoadTexture (yellowTile) error: %s\n", IMG_GetError());
     }
 
+    previewRed = IMG_LoadTexture(renderer, PREVIEW_RED);
+    if (!previewRed) {
+        printf("IMG_LoadTexture (previewRed) error: %s\n", IMG_GetError());
+    }
+    previewYellow = IMG_LoadTexture(renderer, PREVIEW_YELLOW);
+    if (!previewYellow) {
+        printf("IMG_LoadTexture (previewYellow) error: %s\n", IMG_GetError());
+    }
+
     if (TTF_Init() == -1) {
         printf("TTF_Init error: %s\n", TTF_GetError());
     }
-    Hyperspace = TTF_OpenFont("src/Hyperspace.ttf", 24);
+    Hyperspace = TTF_OpenFont("src/Hyperspace.ttf", 24 * window_width / (BASE_TILE_SIZE * WIDTH));
     if (!Hyperspace) {
         printf("TTF_OpenFont error: %s\n", TTF_GetError());
     }
@@ -237,6 +377,8 @@ void endScreen(){
     if (emptyTile) SDL_DestroyTexture(emptyTile);
     if (redTile) SDL_DestroyTexture(redTile);
     if (yellowTile) SDL_DestroyTexture(yellowTile);
+    if (previewRed) SDL_DestroyTexture(previewRed);
+    if (previewYellow) SDL_DestroyTexture(previewYellow);
 
     if (renderer) SDL_DestroyRenderer(renderer);
     if (window) SDL_DestroyWindow(window);
@@ -254,7 +396,6 @@ int main(int argc, char *argv[]){
 #ifdef __EMSCRIPTEN__
     emscripten_set_main_loop(gameLoopFrame, 0, 1);
 #else
-    // desktop fallback: keep the old blocking loop for native
     while(!shouldQuit) {
         gameLoopFrame();
         SDL_Delay(1000/FPS);
